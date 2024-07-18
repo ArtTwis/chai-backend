eefe597ae4246477a70261d7ff1b0c033aafe3d5
Art_Twis <34947496+ArtTwis@users.noreply.github.com>
2024-07-18 13:11:36 +0530

    add await before db operation

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -211,7 +211,7 @@ export const reGenerateAccessToken = asyncHandler(async (req, res) => {
 export const changeCurrentPassword = asyncHandler(async (req, res) => {
   const { oldPassword, newPassword } = req.body;
 
-  const user = User.findById(req.user?._id);
+  const user = await User.findById(req.user?._id);
 
   const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
 
